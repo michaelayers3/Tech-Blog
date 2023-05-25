eb88cebfb8d323077fcfde502182ca199dc64243
michaelayers3 <michaelgregoryayers@gmail.com>
2023-05-25 15:29:46 -0600

    started to fill code out with standard format. going to install some dependencies, then begin with handle bars.

--- models/comments.js
+++ models/comments.js
@@ -1,3 +1,2 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Post = require('./post');
+const Comment = require('./comments');
+
+// Post.hasMany(Comment, {
+//     foreignKey: 'post_id',
+// });
+
+// Comment.belongsTo(Post, {
+//     foreignKey: 'post_id',
+// });
+
+// module.exports = { User, Post, Comment };  
